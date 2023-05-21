059a8d3b2b8c939325a2e4adbf944af4f3c5340a
zhentosxcvt <61989079+noscope096@users.noreply.github.com>
2023-05-21 14:03:48 +0300

    zalupa govno siski

--- js/getIP.js
+++
@@ -1,7 +0,0 @@
-// Получение информации о местоположении на основе IP-адреса
-fetch("http://ip-api.com/json")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data.city); // Город
-    })
-    .catch(console.error);
