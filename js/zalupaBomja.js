3b700f1409e7693db833eda20ab71d7a20961cb4
zhentosxcvt <61989079+noscope096@users.noreply.github.com>
2023-05-24 23:50:15 +0300

    ya pidaras

---
+++ js/zalupaBomja.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button-container"); // находим кнопку для открытия/закрытия окна навигации
+const outside = document.getElementById("particles-container"); // находим окно навигации
+
+outside.addEventListener("click", () => {
+    alert("ты че, ебанатище косорылое, попасть по кнопке не можешь?)");
+});
